083a147b60a43ca58313ba280cb87b26abd3052c
viganhoxha <viganhoxha@gmail.com>
2022-03-23 18:55:50 +0100

    first commit

---
+++ src/main/java/Detyra.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Detyra {
+    public static void main(String[] arasdasdags) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Shkraj nje numer prej 100 deri 999: ");
+        double numri = input.nextDouble();
+        double minuta = 60 * numri;
+
+        System.out.println("oret "+ " jane ");
+
+
+
+
+
+    }
+}
+
---
+++ src/main/java/fletoreJava.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class fletoreJava {
+    public static void main(String[] arasdasdags) {
+        public class fletoreJava {
+            public static void main(String[] arasdasdags) {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Shkruaj numrin 1");
+                int numri1 = input.nextInt();
+                System.out.println("Shkruaj numrin 2");
+                int numri2 = input.nextInt();
+                System.out.println("Shkruaj numrin 3");
+                int numri3 = input.nextInt();
+
+                if (numri1 > numri2 && numri1 > numri3) {
+                    System.out.println("Numri me i madh eshte " +numri1);
+                }
+                else if (numri2 > numri1 && numri2 > numri3) {
+                    System.out.println("Numri me i madh eshte " +numri2);
+                }
+                else if (numri3 > numri1 && numri3 > numri2) {
+                    System.out.println("Numri me i madh eshte " +numri3);
+                }
+                else {
+                    System.out.println("Nuk po e di");
+                }
+            }
+        }
+
+
+
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Cili eshte kryeqyteti i Japonise?");
+        System.out.println("a. Tokyo, b. Berlin, c. Bejing");
+
+        Char a = input.nextInt();
+        String b = B;
+        String c = C;
+
+        if (Choice == a ) {
+            System.out.println("Bravo! 3 pike");
+            else ("0 pike")
+
+        }
+    }
+
+
